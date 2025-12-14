d6571a28f90af347b46e7788a2fa673c30d01f5e
林圣杰 <2874146120@qq.com>
2025-12-14 19:55:20 +0800

    fix(pa1-stage2): extend expression evaluation capabilities

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -36,5 +36,12 @@ void isa_watchpoint_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  *success = false;
+  for (int i = 0; i < 32; i++) {
+    if (strcmp(regs[i], s) == 0) {
+      *success = true;
+      return gpr(i);
+    }
+  }
   return 0;
 }
