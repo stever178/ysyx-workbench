c2904c9ced4cb52a8c7a9189bf5ae726da25d016
林圣杰 <2874146120@qq.com>
2026-01-04 19:03:19 +0800

    fix(pa2.1): implement some `klib` functions
    - pass all tests in `am-kernels/tests/cpu-tests`

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,12 +9,257 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
+static int _itoa(long value, char *str, int base, int is_signed) {
+  if (base < 2 || base > 36) {
+    str[0] = '\0';
+    return 0;
+  }
+
+  char *ptr = str;
+  char *head = str;
+  
+  int is_negative = 0;
+  unsigned long uvalue;
+  if (is_signed && value < 0 && base == 10) {
+    is_negative = 1;
+    uvalue = (unsigned long)(-value);
+  } else {
+    uvalue = (unsigned long)value;
+  }
+
+  do {
+    int digit = uvalue % base;
+    *ptr++ = (digit < 10) ? '0' + digit : 'a' + digit - 10;
+    uvalue /= base;
+  } while (uvalue > 0);
+
+  if (is_negative) {
+    *ptr++ = '-';
+  }
+  *ptr = '\0';
+
+  int len = ptr - head;
+  
+  ptr--;
+  char temp;
+  while (head < ptr) {
+    temp = *head;
+    *head++ = *ptr;
+    *ptr-- = temp;
+  }
+
+  return len;
+}
+
+static int _ftoa(double value, char *str, int precision) {
+  if (precision < 0)
+    precision = 6;
+
+  int is_negative = 0;
+  if (value < 0) {
+    is_negative = 1;
+    value = -value;
+  }
+
+  char *ptr = str;
+
+  long int_part = (long)value;
+  double frac_part = value - int_part;
+
+  if (is_negative) {
+    *ptr++ = '-';
+  }
+
+  int int_len = _itoa(int_part, ptr, 10, 0);
+  ptr += int_len;
+
+  if (precision > 0) {
+    *ptr++ = '.';
+
+    for (int i = 0; i < precision; i++) {
+      frac_part *= 10;
+      int digit = (int)frac_part;
+      *ptr++ = '0' + digit;
+      frac_part -= digit;
+    }
+  }
+
+  *ptr = '\0';
+  return ptr - str;
+}
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *dst = out;
+  char buffer[32];
+
+  while (*fmt) {
+    if (*fmt != '%') {
+      *dst++ = *fmt++;
+      continue;
+    }
+
+    fmt++;
+
+    /* todo Flag characters */
+
+    int width = 0;
+    if (*fmt >= '0' && *fmt <= '9') {
+      while (*fmt >= '0' && *fmt <= '9') {
+        width = width * 10 + (*fmt - '0');
+        fmt++;
+      }
+    }
+
+    int precision = -1;
+    if (*fmt == '.') {
+      fmt++;
+      precision = 0;
+      while (*fmt >= '0' && *fmt <= '9') {
+        precision = precision * 10 + (*fmt - '0');
+        fmt++;
+      }
+    }
+
+    switch (*fmt) {
+    case 'd':
+    case 'i': {
+      int ival = va_arg(ap, int);
+      int len = _itoa(ival, buffer, 10, 1);
+
+      if (width > len) {
+        for (int i = 0; i < width - len; i++) {
+          *dst++ = ' ';
+        }
+      }
+
+      memcpy(dst, buffer, len);
+      dst += len;
+      break;
+    }
+
+    case 'u': {
+      unsigned int uval = va_arg(ap, unsigned int);
+      int len = _itoa(uval, buffer, 10, 0);
+
+      if (width > len) {
+        for (int i = 0; i < width - len; i++) {
+          *dst++ = ' ';
+        }
+      }
+
+      memcpy(dst, buffer, len);
+      dst += len;
+      break;
+    }
+
+    case 'x':
+    case 'X': {
+      unsigned int uval = va_arg(ap, unsigned int);
+      int len = _itoa(uval, buffer, 16, 0);
+
+      if (width > len) {
+        for (int i = 0; i < width - len; i++) {
+          *dst++ = ' ';
+        }
+      }
+
+      memcpy(dst, buffer, len);
+      dst += len;
+      break;
+    }
+
+    case 'f': {
+      double dval = va_arg(ap, double);
+      int len = _ftoa(dval, buffer, precision);
+
+      if (width > len) {
+        for (int i = 0; i < width - len; i++) {
+          *dst++ = ' ';
+        }
+      }
+
+      memcpy(dst, buffer, len);
+      dst += len;
+      break;
+    }
+
+    case 'c': {
+      char c = (char)va_arg(ap, int);
+
+      if (width > 1) {
+        for (int i = 0; i < width - 1; i++) {
+          *dst++ = ' ';
+        }
+      }
+
+      *dst++ = c;
+      break;
+    }
+
+    case 's': {
+      char *s = va_arg(ap, char *);
+      if (!s)
+        s = "(null)";
+      size_t len = strlen(s);
+
+      if (precision >= 0 && len > precision) {
+        len = precision;
+      }
+
+      if (width > len) {
+        for (int i = 0; i < width - len; i++) {
+          *dst++ = ' ';
+        }
+      }
+
+      memcpy(dst, s, len);
+      dst += len;
+      break;
+    }
+
+    case 'p': {
+      void *p = va_arg(ap, void *);
+      unsigned long addr = (unsigned long)p;
+      buffer[0] = '0';
+      buffer[1] = 'x';
+      int len = _itoa(addr, buffer + 2, 16, 0) + 2;
+
+      if (width > len) {
+        for (int i = 0; i < width - len; i++) {
+          *dst++ = ' ';
+        }
+      }
+
+      memcpy(dst, buffer, len);
+      dst += len;
+      break;
+    }
+
+    case '%': {
+      *dst++ = '%';
+      break;
+    }
+
+    default: {
+      *dst++ = '%';
+      *dst++ = *fmt;
+      break;
+    }
+    }
+
+    fmt++;
+  }
+
+  *dst = '\0';
+  return dst - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,176 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+const size_t word_size = sizeof(size_t);
+const size_t align_mask = word_size - 1;
+
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t ret = 0;
+  while (*s != '\0') {
+    ret++;
+    s++;
+  }
+  return ret;
 }
 
+/* The strings src and dst may not overlap. */
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *ret = dst;
+  for (; *src != '\0'; dst++, src++) {
+    *dst = *src;
+  }
+  *dst = *src;
+  return ret;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *ret = dst;
+  for (; n > 0; n--, dst++, src++) {
+    if (*src == '\0') {
+      break;
+    }
+    *dst = *src;
+  }
+  while (n > 0) {
+    *dst = '\0';
+    n--;
+    dst++;
+  }
+  return ret;
 }
 
+/* The strings src and dst may not overlap. */
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *ret = dst;
+  while (*dst != '\0') {
+    dst++;
+  }
+  strcpy(dst, src);
+  return ret;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++) {
+    if (*s1 != *s2) {
+      return *s1 - *s2;
+    }
+  }
+  return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  for (; n > 0 && *s1 != '\0' && *s2 != '\0'; n--, s1++, s2++) {
+    if (*s1 != *s2) {
+      return *s1 - *s2;
+    }
+  }
+  return *s1 - *s2;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  void *ret = s;
+  
+  const char data = (char)c;
+  size_t word = 0;
+  for (size_t i = 0; i < word_size; i++) {
+    word = (word << 8) | data;
+  }
+
+  char *dst = s;
+  for (; n > 0 && ((uintptr_t)dst & align_mask); n--) {
+    *dst++ = data;
+  }
+
+  size_t *word_ptr = (size_t *)dst;
+  for (; n >= word_size; n -= word_size) {
+    *word_ptr++ = word;
+  }
+
+  dst = (char *)word_ptr;
+  for (; n > 0; n--) {
+    *dst++ = data;
+  }
+
+  return ret;
 }
 
+/* The memory areas may overlap.
+DESCRIPTION
+   The memmove() function copies n bytes from memory area src to memory area
+dest. The memory areas may overlap: copying takes place as though the bytes in
+src are first copied into a temporary array that does not overlap src or dest,
+and the bytes are then copied from the temporary array to dest.
+*/
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  void *ret = dst;
+
+  char *dst_ptr = dst;
+  const char *src_ptr = src;
+
+  if (dst < src) {
+    for (; n > 0; n--, dst++, src++) {
+      *dst_ptr = *src_ptr;
+    }
+  } else if (dst > src) {
+    for (size_t i = n - 1; i >= 0; i--) {
+      dst_ptr[i] = src_ptr[i];
+    }
+  }
+
+  return ret;
 }
 
+/* The memory areas must not overlap. */
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  void *ret = out;
+
+  char *dst = out;
+  const char *src = in;
+  for (; n > 0 && ((uintptr_t)dst & align_mask); n--) {
+    *dst++ = *src++;
+  }
+
+  size_t *dst_word_ptr = (size_t *)dst;
+  const size_t *src_word_ptr = (size_t *)src;
+  for (; n >= word_size; n -= word_size) {
+    *dst_word_ptr++ = *src_word_ptr++;
+  }
+
+  dst = (char *)dst_word_ptr;
+  src = (char *)src_word_ptr;
+  for (; n > 0; n--, dst++, src++) {
+    *dst = *src;
+  }
+
+  return ret;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const char *dst = s1;
+  const char *src = s2;
+  for (; n > 0 && ((uintptr_t)dst & align_mask); n--, dst++, src++) {
+    if (*dst != *src) {
+      return *dst - *src;
+    }
+  }
+
+  const size_t *dst_word_ptr = (size_t *)dst;
+  const size_t *src_word_ptr = (size_t *)src;
+  for (; n >= word_size; n -= word_size, dst_word_ptr++, src_word_ptr++) {
+    if (*dst_word_ptr != *src_word_ptr) {
+      return *dst_word_ptr - *src_word_ptr;
+    }
+  }
+
+  dst = (char *)dst_word_ptr;
+  src = (char *)src_word_ptr;
+  for (; n > 0; n--, dst++, src++) {
+    if (*dst != *src) {
+      return *dst - *src;
+    }
+  }
+
+  return 0;
 }
 
 #endif
